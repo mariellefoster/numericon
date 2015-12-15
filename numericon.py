61e5be37f1ccedd84c4e219582cb2613767a7c4b
Marielle Foster <marielleefoster@gmail.com>
2015-12-15 16:28:37 -0600

    adding happiness

--- numericon.py
+++ numericon.py
@@ -1,59 +1,90 @@
+def is_happy(n):
+    h = set()
+    n = str(n)
+
+    while n != "1":
+        if n in h:
+            return False
+        total = 0
+        for i in n:
+            total += int(i)*int(i)
+        h.add(n)
+        n = str(total)
+    return True
+
+
+def bin_rep(n):
+    pass
+
 def main():
-	pass
-	
 
-	###BASIC###
+    inpt = None
+    print "Welcome to Numericon! \nPlease enter in a integer you wish to know more about. \nType q to exit. \n"
 
-		#happy number
+    while inpt != "q":
+        inpt = raw_input("Enter your integer here: ")
 
-		#binary representation
+        while not inpt.isdigit() and inpt != "q":
+            inpt = raw_input("Enter a real integer here (you goofed on your last one: ")
 
+        if inpt != "q": 
+            n = int(inpt)
+        
 
-	###PRIME###
-	
-		#wilson prime
+        ###BASIC###
 
-		#sophie germain
+            #happy number
+            if is_happy(n):
+                print "%i is Happy" % n
 
-		#prime
-
-		#emirp primes
-	
-	###IF NOT PRIME###
-
-		#prime factorization
-
-		#achilles number (all factors are squares or more)
-
-		#carmichael
-
-		#amicable numbers
-			# Let’s take all the proper divisors of 220 (that is to say, 
-			# all its divisors that leave no remainder, including the number 1, 
-			# and excluding the number itself) and all them up:
-
-		#abundant numbers
-
-		#weird numbers
-
-		#untouchable numbers
-
-		#perfect numbers
+            #binary representation
+            print bin_rep(n)
 
 
-	###OTHER###
+        ###PRIME###
+        
+            #wilson prime
 
-		#reciprocal base ten fraction
+            #sophie germain
 
-		#representation as the sum of two squares
+            #prime
 
-		#repunit, repdigit, and repunit primes
+            #emirp primes
+        
+        ###IF NOT PRIME###
 
-		#narcissistic numbers
+            #prime factorization
 
-	#
+            #achilles number (all factors are squares or more)
+
+            #carmichael
+
+            #amicable numbers
+            # Let's take all the proper divisors of 220 (that is to say, 
+            # all its divisors that leave no remainder, including the number 1, 
+            # and excluding the number itself) and all them up:
+
+            #abundant numbers
+
+            #weird numbers
+
+            #untouchable numbers
+
+            #perfect numbers
 
 
+        ###OTHER###
+
+            #reciprocal base ten fraction
+
+            #representation as the sum of two squares
+
+            #repunit, repdigit, and repunit primes
+
+            #narcissistic numbers
+
+        #
+
+main()
 
 
-	
